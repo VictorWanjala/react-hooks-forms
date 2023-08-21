6a67c675fff045dfb63e291b01b6d6251fcb66b5
Victor <wanjala94vic@gmail.com>
2023-08-22 01:09:57 +0300

    completed assignment

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,14 +1,35 @@
 import React, { useState } from "react";
 
 function Form() {
-  const [firstName, setFirstName] = useState("John");
-  const [lastName, setLastName] = useState("Henry");
+  const [firstName, setFirstName] = useState("Victor");
+  const [lastName, setLastName] = useState("Wanjala");
+  const [newsLetter, setNewsLetter] = useState(false)
+
+
+  function handleNewsletterChange(event) {
+    // .checked, not .value!
+    setNewsLetter(event.target.checked);
+  }  
+  
+function handleFirstNameChange(event){
+  setFirstName(event.target.value)
+}
+
+function handleLastNameChange(event){
+  setLastName(event.target.value)
+}
 
   return (
     <form>
-      <input type="text" value={firstName} />
-      <input type="text" value={lastName} />
+      <input type="text" onChange={handleFirstNameChange} value={firstName} />
+      <input type="text" onChange ={handleLastNameChange} value={lastName} />
       <button type="submit">Submit</button>
+      <input
+        type="checkbox"
+        id="newsLetter"
+        onChange={handleNewsletterChange}
+        checked={newsLetter}
+      />
     </form>
   );
 }
